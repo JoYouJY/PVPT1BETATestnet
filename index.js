6af4247b13e591afb74438d1400ad87ee02687af
Jojo <neeya.blog@gmail.com>
2024-02-29 14:06:22 +0800

    comment rotate

--- index.js
+++ index.js
@@ -521,7 +521,7 @@ setTimeout(switchToFantom, 1000);
 
 
 //Resize Canvas
-
+/*
 document.addEventListener("DOMContentLoaded", function() {
   
   // Add click event listener to the button
@@ -531,7 +531,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 console.log("REFRESHED3");
-
+*/
 var isHorizontal = true;
 function rotateCanvas() {
   console.log("resize");
